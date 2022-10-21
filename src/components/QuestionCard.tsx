544894eaf2fbdb66af1aca1fbbdecdc975ce498c
Brigita Sabutyte <79632556+Brigilets@users.noreply.github.com>
2022-10-21 20:58:36 +0200

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,39 +1,4 @@
-// import React from "react";
-// import {AnswerObject} from '../App'
 
-// type QuestionProps = {
-//     question: string;
-//     answers: string[];
-//     callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
-//     userAnswer: AnswerObject | undefined;
-//     questionNr: number;
-//     totalQuestions: number;
-// }
-
-// const QuestionCard: React.FC<QuestionProps> = ({
-//     question,
-//     answers,
-//     callback,
-//     userAnswer,
-//     questionNr,
-//     totalQuestions}) =>(
-//         <div>
-//             <p className="number">
-//                 Question: {questionNr} / {totalQuestions}
-//             </p>
-//             <p dangerouslySetInnerHTML={{__html: question}}/>
-//             <div>
-//                 {answers?.map((answer)=>(
-//                     <div>
-//                         <button disabled={userAnswer ? true : false} value={answer} onClick={callback}></button>
-//                         <span dangerouslySetInnerHTML={{ __html: answer}} />
-//                         </div>
-//                 ))}
-//             </div>
-//         </div>
-// );
-
-// export default QuestionCard;
 import React from 'react';
 // Types
 import { AnswerObject } from '../App';
